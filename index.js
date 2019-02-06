0b739978782c0e4855fb2065bfa5d7b9270604cc
Harishkumar Gangula <harishg@ilimi.in>
2019-02-06 17:31:52 +0530

    Load questions from content added

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let fs = require("fs");
 var bodyParser = require("body-parser");
 var request = require("request");
 const requestPromise = require("request-promise"); //  'request' npm package with Promise support
+const path = require("path");
 
 var questions;
 var responses = [];
@@ -20,23 +21,15 @@ app.use(bodyParser.json());
 
 app.use("/", express.static(__dirname + "/"));
 
-io.on("connection", socket => {});
+io.on("connection", socket => { });
 
-io.on("disconnect", function() {
+io.on("disconnect", function () {
   io.emit("quiz-ended", { quizId: socket.quizId, event: "end" });
 });
 
 app.get("/startQuiz", (req, res) => {
   responses = [];
   questions = JSON.parse(fs.readFileSync("questions.json", "utf-8"));
-  joel = _.find(questions, { identifier: "do_112682561142702080162" });
-  questions = _.filter(questions, function(o) {
-    return o.identifier !== "do_112682561142702080162";
-  });
-  questions = _.shuffle(questions);
-  questions = _.takeRight(questions, 9);
-  questions.unshift(joel);
-  questions = _.shuffle(questions);
   io.emit("startQuiz", {});
   res.end();
 });
@@ -53,11 +46,11 @@ app.get("/endQuiz", (req, res) => {
   var users = _.map(responses, "user");
   users = _.uniqWith(users, _.isEqual);
   results = [];
-  _.forEach(users, function(user) {
+  _.forEach(users, function (user) {
     var userResult = user;
     userResult.time = 0;
     userResult.score = 0;
-    _.forEach(responses, function(response) {
+    _.forEach(responses, function (response) {
       if (user.code === response.user.code) {
         userResult.time = userResult.time + response.option.time;
         if (response.option.isCorrect) {
@@ -83,62 +76,24 @@ app.post("/userResponse", (req, res) => {
   res.end();
 });
 
-app.post("/packet/:id/:periodId", function(req, res) {
+app.post("/packet/:id/:periodId", function (req, res) {
   packetId = req.params.id;
   periodId = req.params.periodId;
   res.status(200).send({ status: true });
 });
 
-app.get("/packet/:studentId", function(req, res) {
+app.get("/packet/:studentId", function (req, res) {
   // Generate attendance event
   res.status(200).send({ id: packetId });
 });
 
-app.delete("/packet", function(req, res) {
+app.delete("/packet", function (req, res) {
   packetId = undefined;
   res.status(200).send({ deleted: true });
 });
 
-const getQuestions = async allQuestionIds => {
-  const questions = allQuestionIds.map(async questionId => {
-    const options = {
-      method: "GET",
-      url: `https://dev.sunbirded.org/action/assessment/v3/items/read/${questionId}`,
-      json: true
-    };
-    const response = await requestPromise(options);
-    return response.result.assessment_item;
-  });
-  return await Promise.all(questions);
-};
-app.use("/get/:id", async (req, res) => {
-  const options = {
-    method: "GET",
-    url: `https://dev.sunbirded.org/api/content/v1/read/${
-      req.params.id
-    }?fields=questions`,
-    json: true
-  };
-  let allQuestions;
-  try {
-    allQuestions = await requestPromise(options);
-    const allQuestionIds = [];
-    allQuestions.result.content.questions.forEach(question => {
-      allQuestionIds.push(question.identifier);
-    });
-    questionMeta = await getQuestions(allQuestionIds);
-    res.json(questionMeta);
-  } catch (error) {
-    console.log(error);
-    if (error.error) {
-      res.status(404).json(error.error);
-    } else {
-      res.status(404).json("unhandled erro");
-    }
-  }
-});
 
-app.post("/telemetry", function(req, res) {
+app.post("/telemetry", function (req, res) {
   var options = {
     method: "POST",
     url: "http://52.172.188.118:3000/v1/telemetry",
@@ -155,15 +110,55 @@ app.post("/telemetry", function(req, res) {
     json: true
   };
   console.log(JSON.stringify(options));
-  request(options, function(error, response, body) {
+  request(options, function (error, response, body) {
     if (error) console.log("telemetry error,", error);
     console.log("telemetry success, ", body);
     res.status(200).send(body);
   });
 });
 
+
+// get questions and store file
+
+
+const getQuestions = async allQuestionIds => {
+  const questions = allQuestionIds.map(async questionId => {
+    const options = {
+      method: "GET",
+      url: `https://dev.ekstep.in/api/assessment/v3/items/read/${questionId}`,
+      json: true
+    };
+    const response = await requestPromise(options);
+    return response.result.assessment_item;
+  });
+  return await Promise.all(questions);
+};
+
+async function loadQuestions(contentId) {
+  const options = {
+    method: "GET",
+    url: `https://dev.ekstep.in/api/content/v3/read/${contentId}?fields=questions`,
+    json: true
+  };
+  let allQuestions;
+  try {
+    allQuestions = await requestPromise(options);
+    const allQuestionIds = [];
+    allQuestions.result.content.questions.forEach(question => {
+      allQuestionIds.push(question.identifier);
+    });
+    questionMeta = await getQuestions(allQuestionIds);
+    console.log(questionMeta);
+    fs.writeFileSync(path.join(__dirname, './questions.json'), JSON.stringify(questionMeta), 'utf8')
+  } catch (error) {
+    console.log('Error while loading questions: ', error);
+  }
+}
+
+loadQuestions('do_112682538715676672114')
+
 var port = process.env.PORT || 3000;
 
-http.listen(port, function() {
+http.listen(port, function () {
   console.log("listening in http://localhost:" + port);
 });
